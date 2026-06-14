dc4849f18569e6d24f84c181c53511405f6cbcb4
purply464 <purply464@gmail.com>
2026-06-15 08:02:57 +0900

    실행테스트

--- plogging-group-maker.php
+++ plogging-group-maker.php
@@ -4,6 +4,20 @@ Plugin Name: Plogging Group Maker
 Description: 플로깅 조 편성 프로그램
 Version: 1.0
 Author: purply464
-*/testdddsf
+*/
 
-add 2 test
+function pgm_admin_page() {
+    echo '<h1>플러그인 동작 확인</h1>';
+}
+
+function pgm_admin_menu() {
+    add_menu_page(
+        '플로깅',
+        '플로깅',
+        'manage_options',
+        'plogging-group-maker',
+        'pgm_admin_page'
+    );
+}
+
+add_action('admin_menu', 'pgm_admin_menu');
